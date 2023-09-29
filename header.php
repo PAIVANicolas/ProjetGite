d97f1a07ede915dcba93f275370342ecb0c0e8e5
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-09-29 20:11:26 +0200

    ajustement de css

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <header>
-    <h1 class="site-name">Votre Site</h1>
-    <div class="language-dropdown">
+    <div class="header">
+        <h1 class="site-name">Votre Site</h1>
         <!-- Insérez ici votre liste déroulante pour la langue -->
-        <select>
+        <select class="select">
             <option value="fr">Fr</option>
             <option value="en">En</option>
             <!-- Ajoutez d'autres langues au besoin -->
         </select>
-    </div>
-    <div class="mode-toggle" onclick="toggleDarkMode()">
-    <button type="button" data-theme-toggle aria-label="Change to dark theme">Change to dark theme</button>
+        <div class="mode-toggle" onclick="toggleDarkMode()">
+            <button type="button" data-theme-toggle aria-label="Change to dark theme">Change to dark theme</button>
+        </div>
     </div>
 </header>
