7192cd2d31965880f3b5436ec95e32d698de8121
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-09-29 16:23:01 +0200

    ajoute d'un header avec son css

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <h1 class="site-name">Votre Site</h1>
+    <div class="language-dropdown">
+        <!-- Insérez ici votre liste déroulante pour la langue -->
+        <select>
+            <option value="fr">Fr</option>
+            <option value="en">En</option>
+            <!-- Ajoutez d'autres langues au besoin -->
+        </select>
+    </div>
+</header>
