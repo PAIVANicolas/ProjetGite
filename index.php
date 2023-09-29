b0c806777834881d1932a88736d1a6d2252163ea
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-09-29 15:40:47 +0200

    Modif chemin accès css

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="icon" href="favicon.ico" />
-    <link href="/css/rules.css" rel="stylesheet"/>
-    <link href="/css/footer.css" rel="stylesheet"/>
+    <link  rel="stylesheet" type="text/css" href="./css/rules.css">
+    <link  rel="stylesheet" type="text/css" href="./css/footer.css"/>
     <title>GITE</title>
 </head>
 <body>
