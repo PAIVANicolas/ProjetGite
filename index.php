16f9f965e23bcb83ebe90af19b13951535188949
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-09-29 14:59:42 +0200

    ajout fichier footer + include dans index

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="favicon.ico" />
+    <link href="/css/rules.css" rel="stylesheet"/>
+    <title>GITE</title>
+</head>
+<body>
+    <nav class="nav-menu">
+        <ul>
+            <li><a href="accueil.html">Accueil</a></li>
+            <li><a href="transport.html">Transport</a></li>
+            <li><a href="activite.html">Activité</a></li>
+            <li><a href="repas.html">Repas</a></li>
+        </ul>
+    </nav>
+</body>
+
+
+<?php include_once("footer.php"); ?>
+
+</html>
