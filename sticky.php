7d89832a741a2b1d2f813a8a04ed0e5233529216
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-09-29 18:43:11 +0200

    ajoute d'un sticky

---
+++ sticky.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="sub-container">
+        <p class="price">À partir de XXX€ / semaine</p>
+        <br>
+        <h2>Contact</h2>
+        <p class="phone">Téléphone : +123456789</p>
+        <p class="email">Email : contact@example.com</p>
+        <button class="reserve-button">Réserver</button>
+    </div>
+</div>
+
