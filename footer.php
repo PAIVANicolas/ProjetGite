7d6170795e58a24b924318f08dc3cc05a0247552
juju <adamij@3il.fr>
2023-09-29 16:20:49 +0200

    mise en forme du footer + ajout infos de base

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
-<footer>
-    <p>Author: Hege Refsnes</p>
-    <p><a href="mailto:hege@example.com">hege@example.com</a></p>
-</footer>
+ <footer class="footerpage">
+     <div class="items">
+         <p class="item">email :<a>beatrice.boyer29@orange.fr</a></p>
+         <p class="item">Numéro de téléphone : +33(0) 6 41 57 73 20</p>
+         <p class="item"><a href="https://www.facebook.com/gitefiguies">facebook</a></p>
+     </div>
+
+
+ </footer>
