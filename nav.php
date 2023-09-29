3c078f2d09b5b854b660753d2f1fc56080d43ad8
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-09-29 16:22:44 +0200

    nav dans le fichier nav.php avec le nav.css

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav class="nav-menu">
+    <ul>
+        <li><a href="accueil.html">Accueil</a></li>
+        <li><a href="transport.html">Transport</a></li>
+        <li><a href="activite.html">Activité</a></li>
+        <li><a href="repas.html">Repas</a></li>
+        <li><a href="apropos.html">A Propos</a></li>
+    </ul>
+</nav>
