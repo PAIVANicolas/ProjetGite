b98bef8c2a415ee6007f0ee52593f5ef886dffd1
juju <adamij@3il.fr>
2023-09-29 19:00:45 +0200

    connexion bdd + credentials

--- bdd/config.php
+++ bdd/config.php
@@ -1,1 +1,14 @@
+<?php
+// Informations d'identification
+require_once('credentials.php');
 
+// Connexion à la base de données MySQL
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Vérifier la connexion
+if($conn === false){
+
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+}
+
+?>
