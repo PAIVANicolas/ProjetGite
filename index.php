37123c14c25e2e1cb2eb597e29626f6f537dff16
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-10-03 13:34:46 +0200

    ajoue des fichiers nécessaire au carousel

--- index.php
+++ index.php
@@ -12,23 +12,28 @@
     <link rel="stylesheet" type="text/css" href="./css/header.css"/>
     <link rel="stylesheet" type="text/css" href="./css/footer.css"/>
     <link rel="stylesheet" type="text/css" href="./css/map.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/carousel.css"/>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
 
     <title>GITE</title>
 </head>
 <body>
 <?php include_once("header.php"); ?>
-
 <?php include_once("nav.php"); ?>
+<?php include_once("carousel.php"); ?>
 
 <?php include_once("sticky.php"); ?>
 
 <div id="map"></div>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 <script src="./js/map.js"></script>
-<?php include_once("footer.php"); ?>
 
+
+<?php include_once("footer.php"); ?>
 <script src="./js/mode.js"></script>
+<script src="./js/carousel.js"></script>
+
+
 </body>
 
 
