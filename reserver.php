d32b880d9aab67326dd29bf0a0690e2f3a375e8b
juju <adamij@3il.fr>
2023-10-03 22:41:22 +0200

    mise en forme du css du formulaire de réservation + ajout du nom dans le formulaire et la BDD

--- reserver.php
+++ reserver.php
@@ -4,59 +4,94 @@ require('bdd/config.php');
 $message = ""; // Pour afficher les erreurs
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $start_date = isset($_POST['start_date']) ? $_POST['start_date'] : null;
-    $end_date = isset($_POST['end_date']) ? $_POST['end_date'] : null;
-    $client_name = isset($_POST['client_name']) ? $_POST['client_name'] : null;
-    $client_email = isset($_POST['client_email']) ? $_POST['client_email'] : null;
-    $client_phone = isset($_POST['client_phone']) ? $_POST['client_phone'] : null;
-    $current_date = date("Y-m-d");
+    if(isset($_POST['submitForm'])) {
+        $start_date = isset($_POST['start_date']) ? $_POST['start_date'] : null;
+        $end_date = isset($_POST['end_date']) ? $_POST['end_date'] : null;
+        $client_name = isset($_POST['client_name']) ? $_POST['client_name'] : null;
+        $client_surname = isset($_POST['client_surname'])?$_POST['client_surname'] : null;
+        $client_email = isset($_POST['client_email']) ? $_POST['client_email'] : null;
+        $client_phone = isset($_POST['client_phone']) ? $_POST['client_phone'] : null;
+        $current_date = date("Y-m-d");
 
 
-    $isValidStartDate = DateTime::createFromFormat('Y-m-d', $start_date) !== false;
-    $isValidEndDate = DateTime::createFromFormat('Y-m-d', $end_date) !== false;
-   
-
-    if (!$start_date || !$end_date || !$client_name || !$client_email || !$client_phone) {
-        $message = "Veuillez remplir tous les champs obligatoires.";
-    } elseif (!$isValidStartDate || $start_date < $current_date) {
-        $message = "La date de début est invalide ou antérieure à la date d'aujourd'hui";
-    } elseif (!$isValidEndDate || $end_date <= $start_date) {
-        $message = "La date de fin est invalide ou elle est antérieure ou égale à la date de début.";
-    } else {
-
-        $stmt = $conn->prepare("INSERT INTO `reservations` (start_date, end_date, client_name, client_email, client_phone, status) VALUES (?, ?, ?, ?, ?, 'en attente')");
+        $isValidStartDate = DateTime::createFromFormat('Y-m-d', $start_date) !== false;
+        $isValidEndDate = DateTime::createFromFormat('Y-m-d', $end_date) !== false;
 
 
-        $stmt->bind_param("sssss", $start_date, $end_date, $client_name, $client_email, $client_phone);
-
-
-        if ($stmt->execute()) {
-            $message = "Réservation soumise avec succès!";
+        if (!$start_date || !$end_date || !$client_name || !$client_email || !$client_phone || !$client_surname) {
+            $message = "Veuillez remplir tous les champs obligatoires.";
+        } elseif (!$isValidStartDate || $start_date < $current_date) {
+            $message = "La date de début est invalide ou antérieure à la date d'aujourd'hui";
+        } elseif (!$isValidEndDate || $end_date <= $start_date) {
+            $message = "La date de fin est invalide ou elle est antérieure ou égale à la date de début.";
         } else {
-            $message = "Erreur lors de la soumission de la réservation: " . $stmt->error;
+
+            $stmt = $conn->prepare("INSERT INTO `reservations` (start_date, end_date, client_name, client_email, client_phone, status, client_surname) VALUES (?, ?, ?, ?, ?, 'en attente',?)");
+
+
+            $stmt->bind_param("ssssss", $start_date, $end_date, $client_name, $client_email, $client_phone,$client_surname);
+
+
+            if ($stmt->execute()) {
+                header("Location: index.php");
+            } else {
+                $message = "Erreur lors de la soumission de la réservation: " . $stmt->error;
+            }
         }
+    }
+    if(isset($_POST['cancelForm'])) {
+        header("Location: index.php");
+        exit();
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="./css/rules.css">
+    <link rel="stylesheet" type="text/css" href="./css/nav.css">
+    <link rel="stylesheet" type="text/css" href="./css/header.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/footer.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/reserverform.css">
+</head>
+<title>Réservation</title>
+
+
+
+<body>
+<?php include_once("header.php"); ?>
+
+
 <form action="" method="POST" class="reservation-form">
+
+    <div class="form-group">
+        <label for="clientName" class="form-label">Nom:</label>
+        <input type="text" id="clientName" name="client_name" class="form-input"  placeholder="Nom" >
+    </div>
+
+    <div class="form-group">
+        <label for="clientSurname" class="form-label">Prénom :</label>
+        <input type="text" id="clientSurname" name="client_surname" class="form-input"  placeholder="Prénom" >
+    </div>
+
     <div class="form-group">
         <label for="startDate" class="form-label">Date de Début:</label>
-        <input type="date" id="startDate" name="start_date" class="form-input" required>
+        <input type="date" id="startDate" name="start_date" class="form-input" >
     </div>
 
     <div class="form-group">
         <label for="endDate" class="form-label">Date de Fin:</label>
-        <input type="date" id="endDate" name="end_date" class="form-input" required>
-    </div>
-
-    <div class="form-group">
-        <label for="clientName" class="form-label">Nom:</label>
-        <input type="text" id="clientName" name="client_name" class="form-input"  placeholder="Nom Prénom" required>
+        <input type="date" id="endDate" name="end_date" class="form-input" disabled="true" >
     </div>
 
     <div class="form-group">
         <label for="clientEmail" class="form-label">Email:</label>
-        <input type="email" id="clientEmail" name="client_email" class="form-input" required>
+        <input type="email" id="clientEmail" name="client_email" class="form-input" >
     </div>
 
     <div class="form-group">
@@ -64,9 +99,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <input type="tel" id="clientPhone" name="client_phone" class="form-input">
     </div>
 
-    <button type="submit" class="submit-button">Soumettre la Réservation</button>
+    <div class="button-group">
+        <button type="submit" name="submitForm"  class="submit">Soumettre la Réservation</button>
+        <button type="cancel" name="cancelForm" class="cancel">Annuler</button>
 
-    <?php if ($message) {
-        echo "<p class='message'>$message</p>";
-    }?>
+
+    </div>
+
+    <div class="messageErreur">
+        <?php if ($message) {
+            echo "<p class='message'>$message</p>";
+        }?>
+    </div>
+
+
 </form>
+
+</body>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const startDateInput = document.getElementById('startDate');
+        const endDateInput = document.getElementById('endDate');
+
+
+        endDateInput.disabled = true;
+
+        const today = new Date().toISOString().split('T')[0];
+        startDateInput.setAttribute('min', today);
+
+
+        startDateInput.addEventListener('change', function() {
+            if (startDateInput.value) {
+
+                endDateInput.disabled = false;
+                endDateInput.setAttribute('min', startDateInput.value);
+            } else {
+
+                endDateInput.disabled = true;
+                endDateInput.value = '';
+            }
+        });
+    });
+</script>
+</html>
