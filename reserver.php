69ddddf09ed0dd71ae51f8e4faa8981820ea5e6e
juju <adamij@3il.fr>
2023-10-03 09:55:58 +0200

    ajout du php pour réserver (gestion des dates contradictoires, des input vides) ajout du lien sur le bouton réserver de index contenu dans sticky

--- reserver.php
+++ reserver.php
@@ -1,4 +1,44 @@
-<form action="/path-to-your-server-endpoint" method="POST" class="reservation-form">
+<?php
+require('bdd/config.php');
+
+$message = ""; // Pour afficher les erreurs
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $start_date = isset($_POST['start_date']) ? $_POST['start_date'] : null;
+    $end_date = isset($_POST['end_date']) ? $_POST['end_date'] : null;
+    $client_name = isset($_POST['client_name']) ? $_POST['client_name'] : null;
+    $client_email = isset($_POST['client_email']) ? $_POST['client_email'] : null;
+    $client_phone = isset($_POST['client_phone']) ? $_POST['client_phone'] : null;
+    $current_date = date("Y-m-d");
+
+
+    $isValidStartDate = DateTime::createFromFormat('Y-m-d', $start_date) !== false;
+    $isValidEndDate = DateTime::createFromFormat('Y-m-d', $end_date) !== false;
+   
+
+    if (!$start_date || !$end_date || !$client_name || !$client_email || !$client_phone) {
+        $message = "Veuillez remplir tous les champs obligatoires.";
+    } elseif (!$isValidStartDate || $start_date < $current_date) {
+        $message = "La date de début est invalide ou antérieure à la date d'aujourd'hui";
+    } elseif (!$isValidEndDate || $end_date <= $start_date) {
+        $message = "La date de fin est invalide ou elle est antérieure ou égale à la date de début.";
+    } else {
+
+        $stmt = $conn->prepare("INSERT INTO `reservations` (start_date, end_date, client_name, client_email, client_phone, status) VALUES (?, ?, ?, ?, ?, 'en attente')");
+
+
+        $stmt->bind_param("sssss", $start_date, $end_date, $client_name, $client_email, $client_phone);
+
+
+        if ($stmt->execute()) {
+            $message = "Réservation soumise avec succès!";
+        } else {
+            $message = "Erreur lors de la soumission de la réservation: " . $stmt->error;
+        }
+    }
+}
+?>
+<form action="" method="POST" class="reservation-form">
     <div class="form-group">
         <label for="startDate" class="form-label">Date de Début:</label>
         <input type="date" id="startDate" name="start_date" class="form-input" required>
@@ -11,7 +51,7 @@
 
     <div class="form-group">
         <label for="clientName" class="form-label">Nom:</label>
-        <input type="text" id="clientName" name="client_name" class="form-input" required>
+        <input type="text" id="clientName" name="client_name" class="form-input"  placeholder="Nom Prénom" required>
     </div>
 
     <div class="form-group">
@@ -25,4 +65,8 @@
     </div>
 
     <button type="submit" class="submit-button">Soumettre la Réservation</button>
+
+    <?php if ($message) {
+        echo "<p class='message'>$message</p>";
+    }?>
 </form>
--- sticky.php
+++ sticky.php
@@ -5,7 +5,6 @@
         <h2>Contact</h2>
         <p class="phone">Téléphone : +123456789</p>
         <p class="email">Email : contact@example.com</p>
-        <button class="reserve-button">Réserver</button>
+        <button class="reserve-button"><a class="submit" href="./reserver.php">Réserver</a></button>
     </div>
 </div>
-
