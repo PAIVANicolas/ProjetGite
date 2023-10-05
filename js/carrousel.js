8751575fcf7deba1ea351c744a9eea57ae088a62
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-10-05 16:33:43 +0200

    Mise a jour du carrousel

---
+++ js/carrousel.js
@@ -0,0 +1,70 @@
+const imgs = document.querySelectorAll('.carrousel-img');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let index = 0;
+
+function layout() {
+  const xOffsetStep = 100;
+  const count = imgs.length;
+  const scaleStep = 0.6;
+  const opacityStep = 0.5;
+
+  for(let i = 0; i < imgs.length; i++) {
+    const img = imgs[i];
+    const sign = Math.sign(i - index);
+
+    let xOffset = (i - index) * xOffsetStep;
+    if(i !== index) {
+      xOffset = xOffset + 80 * sign;
+    }
+    const scale = scaleStep ** Math.abs(i - index);
+    const rotateY = i === index ? 0 : 30 * -sign;
+    img.style.transform = `perspective(800px) translateX(${xOffset}px) scale(${scale}) rotateY(${rotateY}deg)`;
+
+    let opacity = opacityStep ** Math.abs(i - index);
+    if(Math.abs(i - index) > 2) {
+      opacity = 0;
+    }
+    img.style.opacity = opacity;
+
+    img.style.zIndex = count - Math.abs(index - i);
+  }
+}
+
+layout();
+
+function updateClasses() {
+  imgs.forEach((img, i) => {
+    img.classList.remove('active');
+    img.classList.remove('next');
+  });
+
+  imgs[index].classList.add('active');
+
+  if (index < imgs.length - 1) {
+    imgs[index + 1].classList.add('next');
+  } else {
+    imgs[0].classList.add('next');
+  }
+}
+
+updateClasses();
+
+next.addEventListener('click', () => { 
+  index++;
+  if (index >= imgs.length) {
+    index = 0; // Réinitialise l'index à zéro lorsque la dernière image est atteinte
+  }
+  layout();
+  updateClasses();
+});
+
+prev.addEventListener('click', () => { 
+  index--;
+  if (index < 0) {
+    index = imgs.length - 1; // Déplace l'index vers la dernière image lorsque l'utilisateur passe de la première image à la précédente
+  }
+  layout();
+  updateClasses();
+});
