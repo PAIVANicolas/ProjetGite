62602a73d2b10114500a0960b689815e9a68e83a
juju <adamij@3il.fr>
2023-10-10 16:19:02 +0200

    ajout de la page admin et calendar

---
+++ admin_dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="./css/rules.css">
+    <link rel="stylesheet" type="text/css" href="./css/informationsLocation.css">
+    <link rel="stylesheet" type="text/css" href="./css/nav.css">
+    <link rel="stylesheet" type="text/css" href="./css/header.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/footer.css"/>
+    <title>Tableau de bord</title>
+</head>
+
+<body>
+<?php include_once("header.php"); ?>
+<?php include_once("nav.php"); ?>
+
+<?php include_once("calendar.php"); ?>
+
+<?php include_once("footer.php"); ?>
+</body>
+</html>
