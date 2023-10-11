ed091d69f401ca5a0d136f445757fee777eddba8
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-10-11 09:25:39 +0200

    refont

--- index.php
+++ index.php
@@ -6,36 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="icon" href="favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="./css/rules.css">
-    <link rel="stylesheet" type="text/css" href="./css/informationsLocation.css">
-    <link rel="stylesheet" type="text/css" href="./css/nav.css">
-    <link rel="stylesheet" type="text/css" href="./css/header.css"/>
-    <link rel="stylesheet" type="text/css" href="./css/footer.css"/>
-    <link rel="stylesheet" type="text/css" href="./css/map.css"/>
-    <link rel="stylesheet" type="text/css" href="./css/carrousel.css"/>
+    <link rel="stylesheet" type="text/css" href="./assets/css/rules.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/informationsLocation.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/nav.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/header.css"/>
+    <link rel="stylesheet" type="text/css" href="./assets/css/footer.css"/>
+    <link rel="stylesheet" type="text/css" href="./assets/css/map.css"/>
+    <link rel="stylesheet" type="text/css" href="./assets/css/carrousel.css"/>
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Gîte Figuiès</title>
 </head>
 <body>
-<?php include_once("header.php"); ?>
-<?php include_once("nav.php"); ?>
-<?php include_once("carrousel.php"); ?>
+<?php include_once("./php/header.php"); ?>
+<?php include_once("./php/nav.php"); ?>
+<?php include_once("./php/carrousel.php"); ?>
 
 
 
-<?php include_once("informationsLocation.php"); ?>
+<?php include_once("./php/informationsLocation.php"); ?>
 
 <div class="container-map">
     <div id="map"></div>
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-    <script src="./js/map.js"></script>
+    <script src="./assets/js/map.js"></script>
 </div>
 
-<?php include_once("footer.php"); ?>
-<script src="./js/mode.js"></script>
-<script src="./js/carrousel.js"></script>
+<?php include_once("./php/footer.php"); ?>
+<script src="./assets/js/mode.js"></script>
+<script src="./assets/js/carrousel.js"></script>
 
 
 </body>
---
+++ php/header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="header">
+        <h1 class="">Figuiès</h1>
+        <!-- Insérez ici votre liste déroulante pour la langue -->
+        <div class="sub-header">
+            <select class="select">
+                <option value="fr">Fr</option>
+                <option value="en">En</option>
+                <!-- Ajoutez d'autres langues au besoin -->
+            </select>
+            <div class="mode-toggle" onclick="toggleDarkMode()">
+                <button type="button" data-theme-toggle aria-label="Change to dark theme">Change to dark theme</button>
+            </div>
+        </div>
+    </div>
+</header>
