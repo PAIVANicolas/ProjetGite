b8ea95e4abb9db1c32c5fff9c1181973b85ce69a
juju <adamij@3il.fr>
2023-10-18 09:13:53 +0200

    modification du css du calendrier et refactoring des chemins d'accès

--- phpAdmin/update_reservation.php
+++ phpAdmin/update_reservation.php
@@ -1,23 +1,36 @@
 <?php
-// Connexion à la base de données
-require('bdd/config.php');
+require('../assets/bdd/config.php');
+session_start();
+if (isset($_POST['id'], $_POST['status'], $_POST['start_time'], $_POST['end_time'])) {
 
+    $id = $_POST['id'];
+    $status = $_POST['status'];
+    $startTime = $_POST['start_time'];
+    $endTime = $_POST['end_time'];
 
-$id = $_POST['id'];
-$status = $_POST['status'];
-$startTime = $_POST['start_time'];
-$endTime = $_POST['end_time'];
+    $sql = "UPDATE reservations SET status=?";
 
-$sql = "UPDATE reservations SET status='$status'";
-if ($startTime && $endTime) {
-    $sql .= ", start_date=CONCAT(DATE(start_date), ' ', '$startTime'), end_date=CONCAT(DATE(end_date), ' ', '$endTime')";
-}
-$sql .= " WHERE id=$id";
+    if ($startTime && $endTime) {
+        $sql .= ", start_date=CONCAT(DATE(start_date), ' ', ?), end_date=CONCAT(DATE(end_date), ' ', ?)";
+    }
 
-if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . $conn->error;
+    $sql .= " WHERE id=?";
+
+    $stmt = $conn->prepare($sql);
+
+    if ($startTime && $endTime) {
+        $stmt->bind_param("sssi", $status, $startTime, $endTime, $id);
+    } else {
+        $stmt->bind_param("si", $status, $id);
+    }
+
+    if ($stmt->execute()) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+
+    $stmt->close();
 }
 
 $conn->close();
