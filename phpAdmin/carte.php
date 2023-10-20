fc2593b840d6467fb0835ae90733a78a7d80344a
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-10-20 11:48:58 +0200

    Ajout de carte dans le dashbord

---
+++ phpAdmin/carte.php
@@ -0,0 +1,20 @@
+<button id="reset-button" style="display: none" class="reset-button" onclick="resetCards()">Annuler</button>
+
+<div class="card-container">
+    <div class="card">
+        <div class="card-title">Modification de page</div>
+        <button class="card-button" onclick="expandCard(1)">Ouvrir</button>
+    </div>
+    <div class="card">
+        <div class="card-title">Inserter des images</div>
+        <button class="card-button" onclick="expandCard(2)">Ouvrir</button>
+    </div>
+    <div class="card">
+        <div class="card-title">Gérer les images</div>
+        <button class="card-button" onclick="expandCard(3)">Ouvrir</button>
+    </div>
+</div>
+
+
+
+
--- phpAdmin/dashboard.php
+++ phpAdmin/dashboard.php
@@ -6,32 +6,23 @@
     <?php include_once("../phpAdmin/connectionCheck.php"); ?>
     <link rel="icon" href="favicon.ico" />
     <link rel="stylesheet" type="text/css" href="../assets/css/rules.css">
-    <link rel="stylesheet" type="text/css" href="../assets/css/informationsLocation.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/carte.css">
     <link rel="stylesheet" type="text/css" href="../assets/css/nav.css">
     <link rel="stylesheet" type="text/css" href="../assets/css/header.css"/>
     <link rel="stylesheet" type="text/css" href="../assets/css/footer.css"/>
     <title>Tableau de bord</title>
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
-    <script>
-
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth'
-            });
-            calendar.render();
-        });
-
-    </script>
 </head>
 
 <body>
-
-<?php include_once("../php/header.php"); ?>
-<?php include_once("../phpAdmin/navAdmin.php"); ?>
-
+    <?php include_once("../php/header.php"); ?>
+    <?php include_once("../phpAdmin/navAdmin.php"); ?>
 
 
-<?php include_once("../php/footer.php"); ?>
+    <?php include_once("../phpAdmin/carte.php"); ?>
+
+
+
+    <?php include_once("../php/footer.php"); ?>
+    <script src="../assets/js/carte.js"></script>
 </body>
 </html>
