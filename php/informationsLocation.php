465f634bb0019d3bb66cae1e072ba6fdce9e47d9
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-10-27 16:33:01 +0200

    refont du css pour la disposition des blocs

--- php/informationsLocation.php
+++ php/informationsLocation.php
@@ -1,6 +1,6 @@
 <div class="bloc-general">
 
-    <div class="bloc-categorie">
+    <div class="bloc-categorie first-child">
         <h2 class="title-font title-color">À partir de 550€ / semaine</h2>
 
         <div class="paragraph">
@@ -18,51 +18,56 @@
         <button class="reserve-button"><a class="reserver" href="./reserver.php">Réserver</a></button>
         <button class="expand-button" onclick="toggleExpand()"><i class="fa-solid fa-chevron-down"></i></button>
     </div>
-    <div class="bloc-categorie">
-        <h2 class="title-font title-color">CAPACITÉ</h2>
-        <ul class="bloc-liste custom-ul">
-            <li class="custom-li"><i class="fas fa-user"></i>Personne : 4</li>
-            <li class="custom-li"><i class="fas fa-bed"></i> Chambre : 2</li>
-            <li class="custom-li"><i class="fas fa-users"></i> Personne (maximum) : 4</li>
-        </ul>
-    </div>
 
-    <div class="bloc-categorie">
-        <h2 class="title-font title-color">ÉQUIPEMENTS ET SERVICES</h2>
-        <ul class="bloc-liste custom-ul">
-            <li class="custom-li"><i class="fas fa-dog"></i> Animaux acceptés</li>
-            <li class="custom-li"><i class="fas fa-parking"></i> Parking</li>
-            <li class="custom-li"><i class="fas fa-chair"></i> Terrasse</li>
-            <li class="custom-li"><i class="fas fa-tv"></i> Télévision</li>
-            <!-- ... Ajoutez d'autres éléments ici ... -->
-        </ul>
+    <section class="section">
 
-        <button class="expand-button-service" onclick="toggleExpandService()"><i class="fa-solid fa-chevron-down"></i></button>
-    </div>
+        <div class="bloc-categorie">
+            <h2 class="title-font title-color">CAPACITÉ</h2>
+            <ul class="bloc-liste custom-ul">
+                <li class="custom-li"><i class="fas fa-user"></i>Personne : 4</li>
+                <li class="custom-li"><i class="fas fa-bed"></i> Chambre : 2</li>
+                <li class="custom-li"><i class="fas fa-users"></i> Personne (maximum) : 4</li>
+            </ul>
+        </div>
 
-    <div class="bloc-categorie">
-        <h2 class="title-font title-color">TARIFS</h2>
-        <ul class="bloc-liste custom-ul">
-            <li class="custom-li"><i class="fas fa-calendar-alt"></i> Semaine Moyenne saison : 550€</li>
-            <li class="custom-li"><i class="fas fa-moon"></i> Nuitée Moyenne saison : 85€</li>
-            <li class="custom-li"><i class="fas fa-sun"></i> Semaine Haute Saison : 650€</li>
-            <li class="custom-li"><i class="fas fa-star"></i> Nuitée Haute Saison : 110€</li>
-        </ul>
-    </div>
+        <div class="bloc-categorie">
+            <h2 class="title-font title-color">ÉQUIPEMENTS ET SERVICES</h2>
+            <ul class="bloc-liste custom-ul">
+                <li class="custom-li"><i class="fas fa-dog"></i> Animaux acceptés</li>
+                <li class="custom-li"><i class="fas fa-parking"></i> Parking</li>
+                <li class="custom-li"><i class="fas fa-chair"></i> Terrasse</li>
+                <li class="custom-li"><i class="fas fa-tv"></i> Télévision</li>
+                <!-- ... Ajoutez d'autres éléments ici ... -->
+            </ul>
 
-    <div class="bloc-categorie">
-        <h2 class="title-font title-color">MOYENS DE PAIEMENT</h2>
-        <ul class="bloc-liste custom-ul">
-            <li class="custom-li"><i class="fas fa-money-check"></i> Chèque</li>
-            <li class="custom-li"><i class="fas fa-hand-holding-usd"></i> Espèce</li>
-            <li class="custom-li"><i class="fas fa-university"></i> Virements</li>
-        </ul>
-    </div>
+            <button class="expand-button-service" onclick="toggleExpandService()"><i class="fa-solid fa-chevron-down"></i></button>
+        </div>
 
-    <div class="bloc-categorie">
-        <h2 class="title-font title-color">DISPONIBILITÉS</h2>
-        <p><i class="fas fa-calendar-alt"></i> Ouverture à partir du <strong>01/04/2023</strong> jusqu'au <strong>30/10/2023</strong></p>
-    </div>
+        <div class="bloc-categorie">
+            <h2 class="title-font title-color">TARIFS</h2>
+            <ul class="bloc-liste custom-ul">
+                <li class="custom-li"><i class="fas fa-calendar-alt"></i> Semaine Moyenne saison : 550€</li>
+                <li class="custom-li"><i class="fas fa-moon"></i> Nuitée Moyenne saison : 85€</li>
+                <li class="custom-li"><i class="fas fa-sun"></i> Semaine Haute Saison : 650€</li>
+                <li class="custom-li"><i class="fas fa-star"></i> Nuitée Haute Saison : 110€</li>
+            </ul>
+        </div>
+
+        <div class="bloc-categorie">
+            <h2 class="title-font title-color">MOYENS DE PAIEMENT</h2>
+            <ul class="bloc-liste custom-ul">
+                <li class="custom-li"><i class="fas fa-money-check"></i> Chèque</li>
+                <li class="custom-li"><i class="fas fa-hand-holding-usd"></i> Espèce</li>
+                <li class="custom-li"><i class="fas fa-university"></i> Virements</li>
+            </ul>
+        </div>
+
+        <div class="bloc-categorie">
+            <h2 class="title-font title-color">DISPONIBILITÉS</h2>
+            <p><i class="fas fa-calendar-alt"></i> Ouverture à partir du <strong>01/04/2023</strong> jusqu'au <strong>30/10/2023</strong></p>
+        </div>
+
+    </section>
 
 </div>
 
