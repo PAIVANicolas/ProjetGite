64705b4015baadd6371caacc52e3825ca5a43187
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-10-28 03:00:55 +0200

    Mise en place des variables de color

--- php/informationsLocation.php
+++ php/informationsLocation.php
@@ -24,20 +24,20 @@
         <div class="bloc-categorie">
             <h2 class="title-font title-color">CAPACITÉ</h2>
             <ul class="bloc-liste custom-ul">
-                <li class="custom-li"><i class="fas fa-user"></i>Personne : 4</li>
-                <li class="custom-li"><i class="fas fa-bed"></i> Chambre : 2</li>
-                <li class="custom-li"><i class="fas fa-users"></i> Personne (maximum) : 4</li>
+                <li class="custom-li"><i class="fas fa-user icone"></i>Personne : 4</li>
+                <li class="custom-li"><i class="fas fa-bed icone"></i> Chambre : 2</li>
+                <li class="custom-li"><i class="fas fa-users icone"></i> Personne (maximum) : 4</li>
             </ul>
         </div>
 
         <div class="bloc-categorie">
             <h2 class="title-font title-color">ÉQUIPEMENTS ET SERVICES</h2>
             <ul class="bloc-liste custom-ul">
-                <li class="custom-li"><i class="fas fa-dog"></i> Animaux acceptés</li>
-                <li class="custom-li"><i class="fas fa-parking"></i> Parking</li>
-                <li class="custom-li"><i class="fas fa-chair"></i> Terrasse</li>
-                <li class="custom-li"><i class="fas fa-tv"></i> Télévision</li>
-                <!-- ... Ajoutez d'autres éléments ici ... -->
+                <li class="custom-li"><i class="fas fa-dog icone"></i> Animaux acceptés</li>
+                <li class="custom-li"><i class="fas fa-parking icone"></i> Parking</li>
+                <li class="custom-li"><i class="fas fa-chair icone"></i> Terrasse</li>
+                <li class="custom-li"><i class="fas fa-tv icone"></i> Télévision</li>
+                
             </ul>
 
             <button class="expand-button-service" onclick="toggleExpandService()"><i class="fa-solid fa-chevron-down"></i></button>
@@ -46,25 +46,25 @@
         <div class="bloc-categorie">
             <h2 class="title-font title-color">TARIFS</h2>
             <ul class="bloc-liste custom-ul">
-                <li class="custom-li"><i class="fas fa-calendar-alt"></i> Semaine Moyenne saison : 550€</li>
-                <li class="custom-li"><i class="fas fa-moon"></i> Nuitée Moyenne saison : 85€</li>
-                <li class="custom-li"><i class="fas fa-sun"></i> Semaine Haute Saison : 650€</li>
-                <li class="custom-li"><i class="fas fa-star"></i> Nuitée Haute Saison : 110€</li>
+                <li class="custom-li"><i class="fas fa-calendar-alt icone"></i> Semaine Moyenne saison : 550€</li>
+                <li class="custom-li"><i class="fas fa-moon icone"></i> Nuitée Moyenne saison : 85€</li>
+                <li class="custom-li"><i class="fas fa-sun icone"></i> Semaine Haute Saison : 650€</li>
+                <li class="custom-li"><i class="fas fa-star icone"></i> Nuitée Haute Saison : 110€</li>
             </ul>
         </div>
 
         <div class="bloc-categorie">
             <h2 class="title-font title-color">MOYENS DE PAIEMENT</h2>
             <ul class="bloc-liste custom-ul">
-                <li class="custom-li"><i class="fas fa-money-check"></i> Chèque</li>
-                <li class="custom-li"><i class="fas fa-hand-holding-usd"></i> Espèce</li>
-                <li class="custom-li"><i class="fas fa-university"></i> Virements</li>
+                <li class="custom-li"><i class="fas fa-money-check icone"></i> Chèque</li>
+                <li class="custom-li"><i class="fas fa-hand-holding-usd icone"></i> Espèce</li>
+                <li class="custom-li"><i class="fas fa-university icone"></i> Virements</li>
             </ul>
         </div>
 
         <div class="bloc-categorie">
-            <h2 class="title-font title-color">DISPONIBILITÉS</h2>
-            <p><i class="fas fa-calendar-alt"></i> Ouverture à partir du <strong>01/04/2023</strong> jusqu'au <strong>30/10/2023</strong></p>
+            <h2 class="title-font title-color icone">DISPONIBILITÉS</h2>
+            <p><i class="fas fa-calendar-alt icone"></i> Ouverture à partir du <strong>01/04/2023</strong> jusqu'au <strong>30/10/2023</strong></p>
         </div>
 
     </section>
