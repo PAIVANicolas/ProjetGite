d06c7c17d222b4e2f7169c271156b756a970905c
Julien ADAMI <adamij@3il.fr>
2023-10-30 15:53:21 +0100

    corriger les

--- reserver.php
+++ reserver.php
@@ -80,35 +80,9 @@
 </body>
 
 <script src="./assets/js/calendrierVisiteur.js"></script>
-<<<<<<< Updated upstream
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const startDateInput = document.getElementById('startDate');
-        const endDateInput = document.getElementById('endDate');
-
-
-        endDateInput.disabled = true;
-
-        const today = new Date().toISOString().split('T')[0];
-        startDateInput.setAttribute('min', today);
-
-
-        startDateInput.addEventListener('change', function() {
-            if (startDateInput.value) {
-
-                endDateInput.disabled = false;
-                endDateInput.setAttribute('min', startDateInput.value);
-            } else {
-
-                endDateInput.disabled = true;
-                endDateInput.value = '';
-            }
-        });
-    });
-</script>
-=======
 <script src="./assets/js/mode.js"></script>
 <script src="./assets/js/dateInputValidation.js"></script>
->>>>>>> Stashed changes
+
+
 
 </html>
