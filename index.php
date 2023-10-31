eda0fac63208d784e42dbb3212335f37a5a2013d
Julien ADAMI <adamij@3il.fr>
2023-10-31 09:00:53 +0100

    diminution de la taille du favico

--- index.php
+++ index.php
@@ -27,9 +27,8 @@
 <?php include_once("./php/informationsLocation.php"); ?>
 
 <div class="container-map">
-    <div id="map"></div>
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-    <script src="./assets/js/map.js"></script>
+    <div id="map" style="display:none;"></div>
+    <button id="loadMapButton">Afficher la Carte</button>
 </div>
 
 <?php include_once("./php/footer.php"); ?>
@@ -41,3 +40,40 @@
 </body>
 
 </html>
+
+<script>
+    document.getElementById('loadMapButton').addEventListener('click', function() {
+        if (!window.mapLoaded) {
+            loadLeafletScript();
+            window.mapLoaded = true;
+        }
+        document.getElementById('map').style.display = 'block';
+    });
+
+    function loadLeafletScript() {
+        var script = document.createElement('script');
+        script.src = 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js';
+        script.onload = initializeMap;
+        document.head.appendChild(script);
+    }
+
+
+    function initializeMap() {
+        var map = L.map('map').setView([44.449001, 2.493300], 11);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        var marker = L.marker([44.449001, 2.493300])
+            .addTo(map)
+            .bindPopup("<b>Gîte Figuiès</b><br>140 rue de Figuiès<br>12330 Salles-la-Source");
+
+        map.locate({setView: false, maxZoom: 16});
+        map.on('locationfound', function(e) {
+            L.marker(e.latlng).addTo(map)
+                .bindPopup("Vous êtes ici !").openPopup();
+        });
+    }
+
+</script>
