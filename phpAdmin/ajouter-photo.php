52a55007c8c5bcd3cfd47a9034d5b02b3190cb07
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-11-02 20:58:04 +0100

    refonte de css du formulaire pour ajouter des photos

--- phpAdmin/ajouter-photo.php
+++ phpAdmin/ajouter-photo.php
@@ -3,21 +3,27 @@
     <form id="imageForm">
         <div class="form-img">
             <div class="form-group">
-                <label for="section">Section :</label>
-                <select id="section" name="section">
-                    <?php include_once ('../phpAdmin/afficher-sections-image.php');?>
+                <div class="label-container">
+                    <label for="section">Section :</label>
+                </div>
+                <select id="section" name="section" class="select-section">
+                    <?php include_once('../phpAdmin/afficher-sections-image.php');?>
                 </select>
             </div>
             <div class="form-group">
-                <label for="image">Image :</label>
-                <input type="file" id="image" name="image" accept="image/*" required onchange="previewImage(event)">
+                <div class="label-container">
+                    <label for="image">Image :</label>
+                </div>
+                <input type="file" id="image" name="image" accept="image/*" required onchange="previewImage(event)" class="input-image">
             </div>
 
             <div class="form-group">
-                <label for="imageAlt">Description de l'image :</label>
-                <input type="text" id="imageAlt" name="image_alt" required>
+                <div class="label-container">
+                    <label for="imageAlt">Description de l'image :</label>
+                </div>
+                <input type="text" id="imageAlt" name="image_alt" required class="input-description">
             </div>
-            <button type="submit">Ajouter l'image</button>
+            <button type="submit" class="submit-button">Ajouter l'image</button>
         </div>
     </form>
 </div>
