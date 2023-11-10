2de0eb711c8d50bc55912633f0f81dbbd1490370
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-11-10 14:38:13 +0100

    Ajout de label pour les noms

--- phpAdmin/afficher-contact.php
+++ phpAdmin/afficher-contact.php
@@ -1,7 +1,10 @@
 <form method="post" id="contactForm" >
-    Email: <input type="email" name="email" id="email" class="input-texte"><br>
-    Téléphone: <input type="text" name="phone" id="phone" class="input-texte"><br>
-    Facebook: <input type="url" name="facebook" id="facebook" placeholder="URL de votre profil Facebook" class="input-texte"><br>
+    <label class="title-form">Email:</label>
+        <input type="email" name="email" id="email" class="input-texte"><br>
+    <label class="title-form">Téléphone:</label>
+        <input type="text" name="phone" id="phone" class="input-texte"><br>
+    <label class="title-form">Facebook:</label>
+        <input type="url" name="facebook" id="facebook" placeholder="URL de votre profil Facebook" class="input-texte"><br>
     <input type="submit" value="Mettre à jour" >
 </form>
 
