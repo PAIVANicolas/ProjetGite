08282115e93417dd7c0d66824af832413382a677
PAIVANicolas <75265504+PAIVANicolas@users.noreply.github.com>
2023-11-11 22:32:40 +0100

    Externalisation du JS pour le login

---
+++ assets/js/login.js
@@ -0,0 +1,32 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    let formData = new FormData(this);
+    fetch('/ProjetGite/phpAdmin/loginaction.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(text => {
+                    throw new Error(text);
+                });
+            }
+            return response.text();
+        })
+        .then(text => {
+            if (text.includes('dashboard.php')) {
+                window.location.href = '/ProjetGite/phpAdmin/dashboard.php';
+            } else {
+                const errorMsg = document.querySelector('.errorMessage');
+                errorMsg.textContent = "Le mot de passe ou le nom d'utilisateur est incorrect.";
+                errorMsg.style.display = 'block';
+            }
+        })
+        .catch(error => {
+            window.alert(error);
+        });
+});
+
+document.getElementById("cancelButton").addEventListener("click", function() {
+    window.location.href = '/ProjetGite/index.php';
+});
--- login.php
+++ login.php
@@ -3,49 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/ProjetGite/favicon.ico"/>
     <link rel="stylesheet" href="/ProjetGite/assets/css/loginform.css">
 </head>
 <body>
-<form id="loginForm" class="box" method="post" name="login">
-    <div class="cadreLogin" >
-        <h2 class="title">Se connecter</h2>
-        <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
-        <input type="password" class="box-input" name="password" placeholder="Mot de passe">
-        <input type="submit" value="Connexion " name="submit" class="box-button">
-    </div>
-    <?php if (! empty($message)) { ?>
-        <p class="errorMessage"><?php echo $message; ?></p>
-    <?php } ?>
-</form>
-<script>
-    document.getElementById("loginForm").addEventListener("submit", function(e) {
-        e.preventDefault();
-        let formData = new FormData(this);
-        fetch('/ProjetGite/phpAdmin/loginaction.php', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => {
-                if (!response.ok) {
-                    return response.text().then(text => {
-                        throw new Error(text);
-                    });
-                }
-                return response.text();
-            })
-            .then(text => {
-                if (text.includes('dashboard.php')) {
-                    window.location.href = '/ProjetGite/phpAdmin/dashboard.php';
-                } else {
-                    const errorMsg = document.querySelector('.errorMessage');
-                    errorMsg.textContent = "Le mot de passe ou le nom d'utilisateur est incorrect.";
-                    errorMsg.style.display = 'block';
-                }
-            })
-            .catch(error => {
-                 window.alert(error);
-            });
-    });
-</script>
+    <form id="loginForm" class="box" method="post" name="login">
+        <div class="cadreLogin">
+            <h2 class="title">Se connecter</h2>
+            <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
+            <input type="password" class="box-input" name="password" placeholder="Mot de passe">
+            <input type="submit" value="Connexion " name="submit" class="box-button">
+            <button type="button" name="cancel" id="cancelButton">Vision des utilisateurs</button>
+        </div>
+        <?php if (!empty($message)) { ?>
+            <p class="errorMessage"><?php echo $message; ?></p>
+        <?php } ?>
+    </form>
+
+    <script src="/ProjetGite/assets/js/login.js"></script>
 </body>
 </html>
