2f2fa88bb04f70f3b8a26501f78e3a589fe66fed
Julien ADAMI <adamij@3il.fr>
2023-11-11 16:53:56 +0100

    ajout bouton de déconnexion

--- includes/navAdmin.php
+++ includes/navAdmin.php
@@ -2,5 +2,7 @@
     <ul>
         <li><a href="/ProjetGite/phpAdmin/dashboard.php">Tableau de bord</a></li>
         <li><a href="/ProjetGite/phpAdmin/calendar.php">Calendrier</a> </li>
+        <li><button>Déconnexion</button></li>
     </ul>
+
 </nav>
