ae153b09709aea411921db358e03fa37d2f34f55
Julien ADAMI <adamij@3il.fr>
2023-11-12 15:17:00 +0100

    ajout du a propos handler pour récup les infos du xml

--- php/apropos.php
+++ php/apropos.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Page regroupant les informations pour nous contacter">
     <meta name="keywords" content="Contact email réserver gite gîte conques conces conqes maison vacances ">
-    <link rel="icon" href="/ProjetGite/favicon.ico" />
+    <link rel="icon" href="favicon.ico" />
     <link rel="stylesheet" type="text/css" href="/ProjetGite/assets/css/rules.css">
     <link rel="stylesheet" type="text/css" href="/ProjetGite/assets/css/nav.css">
     <link rel="stylesheet" type="text/css" href="/ProjetGite/assets/css/header.css"/>
@@ -20,9 +20,7 @@
 <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/ProjetGite/includes/nav.php");?>
 
 <div class="container">
-    <div class="">
 
-    </div>
 </div>
 
 
@@ -35,3 +33,49 @@
 
 </html>
 
+<!DOCTYPE html>
+<html lang="fr" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Page À propos de notre gîte">
+    <meta name="keywords" content="gîte, maison, vacances, histoire, équipe">
+    <link rel="icon" href="/ProjetGite/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="/ProjetGite/assets/css/rules.css">
+    <link rel="stylesheet" type="text/css" href="/ProjetGite/assets/css/nav.css">
+    <link rel="stylesheet" type="text/css" href="/ProjetGite/assets/css/header.css"/>
+    <link rel="stylesheet" type="text/css" href="/ProjetGite/assets/css/toggle.css"/>
+    <link rel="stylesheet" type="text/css" href="/ProjetGite/assets/css/footer.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>À propos</title>
+</head>
+<body>
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . "/ProjetGite/includes/header.php"); ?>
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . "/ProjetGite/includes/nav.php");?>
+
+<div class="container">
+    <div id="about-content">
+
+    </div>
+</div>
+
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . "/ProjetGite/includes/footer.php"); ?>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        fetchAboutContent();
+    });
+
+    function fetchAboutContent() {
+
+        fetch('/ProjetGite/php/apropos-handler.php')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('about-content').innerHTML = data.content;
+            })
+            .catch(error => console.error('Erreur:', error));
+    }
+</script>
+<script src="/ProjetGite/assets/js/mode.js"></script>
+</body>
+</html>
